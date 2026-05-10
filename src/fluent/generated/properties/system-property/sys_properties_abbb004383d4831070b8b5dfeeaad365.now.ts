4210422d8dad3cdf4b6ddde21ff5912f14414cbb
Tyler <developer@tbcproductions.net>
2026-05-10 20:20:28 +1000

    Set install method for system properties and code action UI script

--- src/fluent/generated/properties/system-property/sys_properties_abbb004383d4831070b8b5dfeeaad365.now.ts
+++ src/fluent/generated/properties/system-property/sys_properties_abbb004383d4831070b8b5dfeeaad365.now.ts
@@ -2,6 +2,7 @@ import { Property } from '@servicenow/sdk/core'
 
 Property({
     $id: Now.ID['abbb004383d4831070b8b5dfeeaad365'],
+    $meta: { installMethod: 'first install' },
     name: 'monaco.plus.widget.related_list_exclusions',
     value: '',
     description:
